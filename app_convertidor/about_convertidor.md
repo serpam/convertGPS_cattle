14a46f6fd6bc9662417e328c7e0d52f03f9238a2
ajpelu <ajpelu@gmail.com>
2022-11-22 15:59:23 +0100

    modify convertidor para incluir ayuda y about

---
+++ app_convertidor/about_convertidor.md
@@ -0,0 +1,28 @@
+## Sobre la app
+
+Esta aplicación convierte datos de seguimiento GPS de ganado de tomados con dispositivos de medición contínua que registran la posición del animal a intervalos de 5 minutos. 
+
+Cada dipositivo almacena los datos en un formato propio que no es adecuado para su inclusión en la base de datos desarrollada para la visualización de los datos de pastoreo, que posteriormente alimentam el Virtual Research Environment (VRE) de estimación de la intensidad de pastoreo ([vreGI](https://serpam.github.io/sumhal_vreGI). 
+
+Esta aplicación se ha desarrollado en el marco del proyecto [SUMHAL](https://lifewatcheric-sumhal.csic.es/) (*Sustainability for Mediterranean Hotspots in Andalusia integrating LifeWatch ERIC*), en concreto en los subproyectos LWE2103026 y LWE2103027 que se centran en analizar el papel de las prácticas silvopastorales tradicionales en la biodiversidad de los ecosistemas y en la prevención de incendios forestales de espacios naturales de Andalucía, para lo que se apoyará en herramientas tecnológicas actualizadas. Estos subpoyectos los están ejecutando el Servicio de Evaluación, Restauración y Protección de Agrosistemas Mediterráneos (***SERPAM***) de la [Estación Experimental del Zaidin](https://www.eez.csic.es/), del CSIC. 
+
+### Como citar 
+
+Pérez-Luque, A.J.; Tognetti Barbieri, M.J.; Ramos-Font, M.E.; Robles-Cruz, A.B. (2022). convertGPS_cattle: Una aplicación para el procesamiento y exploración de datos de dispositivos GPS de medición contínua para el ganado. https://github.com/serpam/convertGPS_cattle. version 1.0. 
+
+### Credits <sup><a href="#fn1" id="ref1">1</a></sup>
+
+-   [**Antonio J. Pérez-Luque**](https://github.com/ajpelu) <a href="https://orcid.org/0000-0002-1747-0469" target="orcid.widget"> <img src="https://info.orcid.org/wp-content/uploads/2019/11/orcid_16x16.png" alt="ORCID logo" width="16" height="16"/></a>: Conceptualization, Data curation, Methodology, Software, Validation, Visualization
+
+-   **Mauro J. Tognetti Barbieri**: Data curation, Methodology, Validation, and Visualization.
+
+-   **Maria Eugenia Ramos Font** <a href="https://orcid.org/0000-0002-4888-0401" target="orcid.widget"> <img src="https://info.orcid.org/wp-content/uploads/2019/11/orcid_16x16.png" alt="ORCID logo" width="16" height="16"/></a>:: Funding acquisition and Resources.
+
+-   **Ana Belén Robles Cruz**: Funding acquisition, Project administration, Resources, and Supervision.
+
+
+<sup id="fn1">1. ([CRedIT](https://credit.niso.org/) Statment)<a href="#ref1" title="Jump back to footnote 1 in the text.">↩</a></sup>
+
+## Acknowledgements 
+Este aplicación se ha desarrollado con financiación del proyecto SUMHAL: *SUMHAL, LIFEWATCH-2019-09-CSIC-13, POPE 2014-2020* 
+
--- app_convertidor/app.R
+++ app_convertidor/app.R
@@ -1,110 +1,132 @@
 library(shiny)
 library(tidyverse)
 library(shinythemes)
+library(shinyhelper)
 library(hms)
 
 # ui object
-ui <- fluidPage(theme = shinytheme("flatly"),
+ui <- fluidPage(
+  theme = shinytheme("flatly"),
 
   # App title ----
   titlePanel(
     title =
-      div("Convertidor de datos de GPS de medida continua al formato estándar",
-    p(),
-    img(src = "/logo_serpam.jpg", height = "50", " SERPAM-EEZ"),
-    tags$a(href = "https://lifewatcheric-sumhal.csic.es/", 
-           target = "_blank", 
-           tags$img(src = "/logosumhal.jpg", height = "70", "Proyecto SUMHAL"))),
+      div(
+        "Convertidor de datos de GPS de medida continua al formato estándar",
+        p(),
+        img(src = "logo_serpam.jpg", height = "50", " SERPAM-EEZ"),
+        tags$a(
+          href = "https://lifewatcheric-sumhal.csic.es/",
+          target = "_blank",
+          tags$img(src = "logosumhal.jpg", height = "70", "Proyecto SUMHAL")
+        )
+      ),
     windowTitle = "GPS Converter"
   ),
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
-    
+
     # Sidebar panel for inputs ----
     sidebarPanel(
-      
+
       # Input: Select a file ----
       fileInput("file1", "Selecciona archivo",
-                multiple = FALSE,
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv")),
-      
+        multiple = FALSE,
+        accept = c(
+          "text/csv",
+          "text/comma-separated-values,text/plain",
+          ".csv"
+        ) 
+      ) %>% 
+        helper(icon = "question",
+               colour = "green",
+               type = "markdown",
+               content = "input_helper_convertidor", 
+               buttonLabel = "Ok"),
+
       # Horizontal line ----
       tags$hr(),
-      
+
       # Input: Checkbox if file has header ----
-      checkboxInput("header", "Encabezamiento?", TRUE),
-      
-      numericInput("skip", "Saltar líneas?", 0, min=0, max=10), 
-      
+      checkboxInput("header", "Encabezamiento?", FALSE),
+      numericInput("skip", "Saltar líneas?", 0, min = 0, max = 10),
+
       # Input: Select separator ----
       radioButtons("sep", "Separador",
-                   choices = c("coma (,)" = ",",
-                               "punto y coma (;)"  = ";",
-                               "tabulador" = "\t"),
-                   selected = ","),
-      
+        choices = c(
+          "coma (,)" = ",",
+          "punto y coma (;)" = ";",
+          "tabulador" = "\t"
+        ),
+        selected = ","
+      ),
+
       # Input: Select quotes ----
       radioButtons("quote", "Quote",
-                   choices = c(None = "",
-                               "Double Quote" = '"',
-                               "Single Quote" = "'"),
-                   selected = '"'),
-      
+        choices = c(
+          None = "",
+          "Double Quote" = '"',
+          "Single Quote" = "'"
+        ),
+        selected = '"'
+      ),
+
       # Horizontal line ----
       tags$hr(),
-      
+
       # Input: Select number of rows to display ----
-      #radioButtons("disp", "Display",
+      # radioButtons("disp", "Display",
       #             choices = c(Head = "head",
       #                         All = "all"),
       #            selected = "head"),
-      
+
       # Button
       downloadButton("downloadData", "Download")
-      
     ),
-    
+
     # Main panel for displaying outputs ----
     mainPanel(
-      
-      # Output: Data file ----
-      tableOutput("contents")
-      
+      tabsetPanel(type = "tabs",
+                 tabPanel("Table", tableOutput("contents")),
+                 tabPanel("About", includeMarkdown("about_convertidor.md")))
     )
-  ))
+  )
+)
 
 # Define server logic to read selected file ----
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  observe_helpers(withMathJax = TRUE)
   
   file_name <- reactive({
     inFile <- input$file1
-    
-    if (is.null(inFile))
+
+    if (is.null(inFile)) {
       return(NULL)
-    
-    return (stringi::stri_extract_first(str = inFile$name, regex = ".*(?=\\.)"))
+    }
+
+    return(stringi::stri_extract_first(str = inFile$name, regex = ".*(?=\\.)"))
   })
-  
-  
-  ##### Selected data and typologies 
+
+
+  ##### Selected data and typologies
   datasetInput <- reactive({
     req(input$file1)
-    
+
     # when reading semicolon separated files,
     # having a comma separator causes `read.csv` to error
     tryCatch(
       {
         dfraw <- read.csv(input$file1$datapath,
-                          header = input$header,
-                          sep = input$sep,
-                          quote = input$quote, 
-                          skip = input$skip)
-        
-        df <- dfraw %>% 
-          mutate(id_gps = file_name()) %>% 
-          dplyr::select_if(~ !all(is.na(.))) %>% 
+          header = input$header,
+          sep = input$sep,
+          quote = input$quote,
+          skip = input$skip
+        )
+
+        df <- dfraw %>%
+          mutate(id_gps = file_name()) %>%
+          dplyr::select_if(~ !all(is.na(.))) %>%
           rename(
             id = V1,
             year = V11,
@@ -116,49 +138,45 @@ server <- function(input, output) {
             lat = V17,
             long = V18
           ) %>%
-          filter(lat != -1 | long != -1) 
-        #%>%
+          filter(lat != -1 | long != -1)
+        # %>%
         # mutate(
         #   date = as.POSIXct(paste(year, month, day, sep = "-"), format = "%y-%m-%d"),
         #   time = hms::as_hms(paste(hour, minute, second, sep = ":"))
         # ) %>%
         # mutate(datetime = as.POSIXct(paste(date, time), format = "%Y-%m-%d %H:%M:%S")) %>%
-        #dplyr::select(id, id_gps, lat, long, datetime, date, time)
-        
+        # dplyr::select(id, id_gps, lat, long, datetime, date, time)
       },
       error = function(e) {
         # return a safeError if a parsing error occurs
         stop(safeError(e))
       }
     )
-    
+
     # return(df)
-    
+
     # if(input$disp == "head") {
     #   return(head(df))
     # }
     # else {
     #   return(df)
     # }
-    # 
+    #
   })
-  
-  
-  
+
+
+
   output$contents <- renderTable({
     datasetInput()
-    
-    
+
+
     # input$file1 will be NULL initially. After the user selects
     # and uploads a file, head of that data file by default,
     # or all rows if selected, will be shown.
-    
-    
-    
   })
-  
 
-  
+
+
   # Downloadable csv of selected dataset ----
   output$downloadData <- downloadHandler(
     filename = function() {
@@ -168,7 +186,6 @@ server <- function(input, output) {
       write.csv(datasetInput(), file, row.names = FALSE)
     }
   )
-  
 }
 
 
